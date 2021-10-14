177ffefafdcadee1f5b6d4b4c8599e0bac1362f9
HeroBrine1st Erquilenne <herobrine1st@pm.me>
2021-10-14 20:12:01 +0300

    Move modules from framework to bot

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    application
+}
+
+group = "ru.herobrine1st.fusion"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven(url="https://m2.dv8tion.net/releases")
+    maven(url="https://jitpack.io")
+}
+
+dependencies {
+    implementation("net.dv8tion:JDA:4.3.0_277")
+    implementation("com.github.HeroBrine1st:Fusion-framework:master-SNAPSHOT")
+    implementation("com.google.code.gson:gson:2.8.7")
+    implementation("com.squareup.okhttp3:okhttp:4.9.0")
+}
+
+configurations.all {
+    resolutionStrategy.cacheChangingModulesFor(0, "seconds")
+}
